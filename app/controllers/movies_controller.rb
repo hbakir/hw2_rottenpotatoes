43002cfe4e613d3296c0439dee320b12305a65f9
hbakir <hicham@bakir.ch>
2012-10-20 20:20:52 +0200

    Added check boxes pre initialization with previous request values.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,13 @@
 class MoviesController < ApplicationController
 
+  def initialize
+    puts "calling MoviesController.initialize"
+    super
+    @all_ratings = Movie.all_ratings
+    @filter = Hash.new
+    @all_ratings.each { |rating| @filter[rating] = "1" }
+  end
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,15 +15,32 @@ class MoviesController < ApplicationController
   end
 
   def index
+    puts "calling MoviesController.index"
+    puts "params = "
+    puts params.inspect
     @sort = params[:sort]
-    @all_ratings = Movie.all_ratings
-    if @sort == 'by_title'
-      @movies = Movie.all.sort { |a,b| a.title <=> b.title }
-    elsif @sort == 'by_release_date'
-      @movies = Movie.all.sort_by &:release_date    
+    ratings = params[:ratings]
+    if ratings == nil
+      puts "ratings == nil => all movies"
+      movies = Movie.all
     else
-      @movies = Movie.all
+      puts "ratings NOT nil => filtered movies"
+      puts "ratings.keys = "
+      puts ratings.keys
+      @filter = ratings
+      movies = Movie.filter_by_ratings(ratings.keys)
     end
+    if @sort == 'by_title'
+      @movies = movies.sort { |a,b| a.title <=> b.title }
+    elsif @sort == 'by_release_date'
+      @movies = movies.sort_by &:release_date    
+    else
+      @movies = movies
+    end
+    puts "@filter = "
+    puts @filter.inspect
+    puts "@all_ratings = "
+    puts @all_ratings.inspect
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,11 @@
 class Movie < ActiveRecord::Base
   def self.all_ratings
     result = []
-    Movie.find_by_sql("SELECT DISTINCT(m.rating) FROM movies m").each {|movie| result << movie.rating}
+    Movie.find_by_sql("SELECT DISTINCT(m.rating) FROM movies m ORDER BY m.rating").each { |movie| result << movie.rating }
     result
+  end
+  
+  def self.filter_by_ratings(filter)
+    Movie.where(:rating => filter)
   end
 end
